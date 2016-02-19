5169218ce73230976d268df8fb5b71aab6351400
Joseph Prachar <josephprachar@comcast.net>
2016-02-19 11:13:51 -0800

    nice n clean

--- counter/counter/counter.sim/sim_1/behav/elaborate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=C:\\Xilinx\\Vivado\\2014.4\\bin
-call %xv_path%/xelab  -wto 13768df7f99448768a2dfc6339ca1f04 -m64 --debug typical --relax -L xil_defaultlib -L secureip --snapshot counter_test_behav xil_defaultlib.counter_test -log elaborate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
--- memory/memory.sim/sim_1/behav/simulate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=C:\\Xilinx\\Vivado\\2015.3\\bin
-call %xv_path%/xsim ScratchPad_TB_behav -key {Behavioral:sim_1:Functional:ScratchPad_TB} -tclbatch ScratchPad_TB.tcl -log simulate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
