2b94a12915e3affeb6a7d23824e804bf9b26762d
Joseph Prachar <josephprachar@comcast.net>
2016-03-09 15:24:03 -0800

    Add calc_line

--- GameOfLife.asm
+++ GameOfLife.asm
@@ -48,6 +48,65 @@ COMPUTE:     MOV R2, 0x00
              ADD R2, 0x01
              CMP R2, GRID_HEIGHT
              BRNE Y_LOOP
+             RET
+
+; Notes about CALC_LINE
+; Yes this is repetitive, but to keep the buffer stored in the registers there
+;needs to be 'concrete' registers for each column of bytes. This could be done
+;using the ScratchRAM by incrementing an address each time this function
+;switches to a new regester
+
+CALC_LINE:   MOV R3, BITSEL_7
+             MOV R22, 0x00
+     FILL22:   CALL COUNT_SUR
+               CALL IS_ALIVE ; Result is put into R4 [Dead=0x00;Alive=0xFF]
+               AND R4, R3
+               OR R22, R4
+               CLC
+               LSR R3
+               BRNE FILL22
+             MOV R3, BITSEL_7
+             MOV R23, 0x00
+     FILL23:   CALL COUNT_SUR
+               CALL IS_ALIVE
+               AND R4, R3
+               OR R23, R4
+               CLC
+               LSR R3
+               BRNE FILL23
+             MOV R3, BITSEL_7
+             MOV R24, 0x00
+     FILL24:   CALL COUNT_SUR
+               CALL IS_ALIVE
+               AND R4, R3
+               OR R24, R4
+               CLC
+               LSR R3
+               BRNE FILL24
+             MOV R3, BITSEL_7
+             MOV R25, 0x00
+     FILL25:   CALL COUNT_SUR
+               CALL IS_ALIVE
+               AND R4, R3
+               OR R25, R4
+               CLC
+               LSR R3
+               BRNE FILL25
+             MOV R3, BITSEL_7
+             MOV R25, 0x00
+     FILL26:   CALL COUNT_SUR
+               CALL IS_ALIVE
+               AND R4, R3
+               OR R26, R4
+               CLC
+               LSR R3
+               BRNE FILL26
+             RET
+
+
+               
+               
+             
 
 
 DONE:        ; Do clean-up work
